3acefecfb35ead48638296217c81577bb6265512
paukasha <odinpozitivchik@mail.ru>
2021-01-29 23:40:44 +1000

    2commit

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HeaderComp } from './Header';
-
-window.addEventListener('load', () => {
-    ReactDOM.render(<HeaderComp />, document.getElementById('react_root'));
-});
---
+++ src/shared/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            <h1>React react123456</h1>
+        </header>
+    );
+}
